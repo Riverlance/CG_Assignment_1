d498c3174813d207e75431291d67ea28dd0c943f
Riverlance <biel94moraes@msn.com>
2019-07-15 17:52:49 -0300

    Updated OpenGL lib from 1.11.0 to 2.1.0

--- CG_Assignment_1/main.cpp
+++ CG_Assignment_1/main.cpp
@@ -1,47 +1,29 @@
-//Jeff Chastine
-#include <Windows.h>
-#include <GL\glew.h>
-#include <GL\freeglut.h>
-#include <iostream>
+#include "main.h"
 
-using namespace std;
-
-void changeViewPort(int w, int h)
+//-----------------------------------------------------------------------------
+void MyGlDraw(void)
 {
-	glViewport(0, 0, w, h);
+	//*************************************************************************
+	// Chame aqui as funções do mygl.h
+	//*************************************************************************
+
+
+
 }
 
-void render()
+//-----------------------------------------------------------------------------
+int main(int argc, char **argv)
 {
-	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	glutSwapBuffers();
-}
+	// Inicializações.
+	InitOpenGL(&argc, argv);
+	InitCallBacks();
+	InitDataStructures();
 
+	// Ajusta a função que chama as funções do mygl.h
+	DrawFunc = MyGlDraw;
 
-
-int main(int argc, char* argv[]) {
-
-	// Initialize GLUT
-	glutInit(&argc, argv);
-	// Set up some memory buffers for our display
-	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-	// Set the window size
-	glutInitWindowSize(800, 600);
-	// Create the window with the title "Hello,GL"
-	glutCreateWindow("Hello, GL");
-	// Bind the two functions (above) to respond when necessary
-	glutReshapeFunc(changeViewPort);
-	glutDisplayFunc(render);
-
-	// Very important!  This initializes the entry points in the OpenGL driver so we can 
-	// call all the functions in the API.
-	GLenum err = glewInit();
-	if (GLEW_OK != err) {
-		fprintf(stderr, "GLEW error");
-		return 1;
-	}
-
-
+	// Framebuffer scan loop.
 	glutMainLoop();
+
 	return 0;
 }
